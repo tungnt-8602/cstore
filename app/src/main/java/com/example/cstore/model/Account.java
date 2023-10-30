ab569a6e1fab0ee0600a95de351017f722361773
n0wUCme <08062002thetung@gmail.com>
2023-10-30 13:55:42 +0700

    Define model

---
+++ app/src/main/java/com/example/cstore/model/Account.java
@@ -0,0 +1,70 @@
+package com.example.cstore.model;
+
+public class Account {
+    private Integer id;
+    private String username;
+    private String password;
+    private String address;
+    private String phone;
+    private String email;
+
+    public Account() {
+    }
+
+    public Account(Integer id, String username, String password, String address, String phone, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- app/src/main/java/com/example/cstore/presentation/store/product/ProductItemAdapter.java
+++ app/src/main/java/com/example/cstore/presentation/store/product/ProductItemAdapter.java
@@ -13,19 +13,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.cstore.R;
-import com.example.cstore.model.Product;
+import com.example.cstore.model.Products;
 
 import java.util.List;
 
 public class ProductItemAdapter extends RecyclerView.Adapter<ProductItemAdapter.ViewHolder> {
 
     private Context context;
-    private List<Product> productArrayList;
+    private List<Products> productsArrayList;
     private OnClickListener onClickListener;
 
-    public ProductItemAdapter(Context context, List<Product> productArrayList) {
+    public ProductItemAdapter(Context context, List<Products> productsArrayList) {
         this.context = context;
-        this.productArrayList = productArrayList;
+        this.productsArrayList = productsArrayList;
     }
 
     @NonNull
@@ -39,7 +39,7 @@ public class ProductItemAdapter extends RecyclerView.Adapter<ProductItemAdapter.
     @SuppressLint("RecyclerView")
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Product p = productArrayList.get(position);
+        Products p = productsArrayList.get(position);
         Glide.with(context).load(p.getThumbnail()).into(holder.productThumb);
         holder.productName.setText(p.getProductName());
         holder.productPrice.setText(p.getPrice().toString());
@@ -47,14 +47,14 @@ public class ProductItemAdapter extends RecyclerView.Adapter<ProductItemAdapter.
             @Override
             public void onClick(View view) {
                 if (onClickListener != null) {
-                    onClickListener.onClick( position, productArrayList.get(position));
+                    onClickListener.onClick( position, productsArrayList.get(position));
                 }
             }
         });
     }
 
     public interface OnClickListener {
-        void onClick(int position, Product p);
+        void onClick(int position, Products p);
     }
 
     public void setOnClickListener(OnClickListener onClickListener) {
@@ -64,7 +64,7 @@ public class ProductItemAdapter extends RecyclerView.Adapter<ProductItemAdapter.
 
     @Override
     public int getItemCount() {
-        return productArrayList.size();
+        return productsArrayList.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
